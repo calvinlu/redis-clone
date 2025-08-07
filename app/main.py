e07269f80ba9578f73c317e92f3b4bc3b2810f61
Calvin Lu <calvinlu2015@gmail.com>
2025-08-07 05:05:30 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 import socket  # noqa: F401
 
 
+PONG = b'+PONG\r\n'
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
+    client_conn, _ = server_socket.accept() # wait for client
 
-    connection.sendall(b'+PONG\r\n')
+    while True: 
+        request: bytes = client_conn.recv(512)
+        data: str = request.decode()
+
+        if 'ping' in data.lower():
+            client_conn.sendall(PONG)
 
     
 
