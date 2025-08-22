8aa641e9f9e2166bcde270ca5365fb391f663f81
Calvin Lu <calvinlu2015@gmail.com>
2025-08-21 19:41:24 -0700

    Implement type command.

---
+++ app/commands/type_command.py
@@ -0,0 +1,41 @@
+"""Implementation of the Redis TYPE command.
+
+"""
+from typing import Any, Optional
+
+from app.store import Store
+
+from .base_command import Command
+
+
+class TypeCommand(Command):
+    """Implementation of the Redis TYPE command.
+
+    The TYPE command is used to return the type of a given key.
+    """
+
+    @property
+    def name(self) -> str:
+        """Return the command name in uppercase."""
+        return "TYPE"
+
+    async def execute(
+        self, *args: Any, store: Optional[Store] = None, **kwargs: Any
+    ) -> str:
+        """Handle TYPE command by returning the type of the given key
+
+        Args:
+            *args: Should contain the given key to check for type.
+            store: The store instance to use for storage.
+
+        Returns:
+            str: The type of the key (e.g., "string", "list", "none")
+        """
+        if len(args) != 1:
+            raise ValueError("ERR wrong number of arguments for 'type' command")
+
+        # Get the type from store (will be one of: "string", "list", "none")
+        return store.type(args[0])
+
+
+command = TypeCommand()
